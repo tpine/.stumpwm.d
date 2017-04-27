0750367e30527ee674904b37bcfddce2b658911a
Thomas <thomas@pinegrove.io>
2017-04-27 12:17:36 +1000

    Add documentation to get-sly-path

--- slynk-fix.lisp
+++ slynk-fix.lisp
@@ -3,7 +3,9 @@
 (defvar *elpa-path* "/home/thomas/.emacs.d/elpa/")
 
 (defun get-sly-path ()
-  "Run script to get sly version number and append it to elpa path"
+  "Run script to get sly version number and append it to elpa path
+We need to do this as elpa stores packages in the format <pkg-name>-<date>.<version>
+This could probably be done using regex in the future"
   (let ((sly-version "sly-"))
     (multiple-value-bind (out err proc) (trivial-shell:shell-command "/home/thomas/.stumpwm.d/slynk-path-fix.sh")
 	(declare (ignore err proc))	
