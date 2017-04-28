ae107ad98c313524946a91223afadd23b1737562
Thomas <thomas@pinegrove.io>
2017-04-28 17:03:16 +1000

    Change how sly's version is gotten.
    Instead of passing a premade script to emacs the macro
    run-emacs-script is added allowing emacs scripts to be written as if
    it is common lisp in line.
    
    This change also allows for get-sly-version to be simplified as emacs's
    output is treated as a list instead of a string.

--- slynk-fix.lisp
+++ slynk-fix.lisp
@@ -2,14 +2,22 @@
 
 (defvar *elpa-path* "/home/thomas/.emacs.d/elpa/")
 
+(defmacro run-emacs-script (&rest body)
+  "Run body as emacs script"
+  (let ((script nil))
+    (loop for sexep in body
+	  do (setf script (concatenate 'string script (string-downcase (format nil "~a" sexep)))))
+    `(multiple-value-bind (out err proc) (trivial-shell:shell-command (format nil "emacs --batch --eval=\"(progn ~a)\"" ,script))
+       (declare (ignore proc))
+       (list :output (read-from-string out) :error err))))
+
 (defun get-sly-path ()
   "Run script to get sly version number and append it to elpa path
 We need to do this as elpa stores packages in the format <pkg-name>-<date>.<version>
 This could probably be done using regex in the future"
-  (let ((sly-version "sly-"))
-    (multiple-value-bind (out err proc) (trivial-shell:shell-command "/home/thomas/.stumpwm.d/slynk-path-fix.sh")
-	(declare (ignore err proc))	
-      (with-input-from-string (ver (remove #\newline (remove #\) (remove #\( out))))
-	(setf sly-version (concatenate 'string sly-version (write-to-string (read ver)) "." (write-to-string (read ver)))))
+  (let ((version (getf (run-emacs-script
+			(package-initialize)
+			(require 'pkg-info)
+			(princ (pkg-info-package-version 'sly))) :output)))
 
-      (concatenate 'string *elpa-path* sly-version "/"))))
+    (concatenate 'string *elpa-path* "sly-" (write-to-string (first version)) "." (write-to-string (second version)) "/")))
