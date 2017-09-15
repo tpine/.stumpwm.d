b09fc79bb6890a693bea77c988efc0f8f0103523
Thomas Atkinson <thomas.atkinson@blacknova.com.au>
2017-09-15 14:22:39 +1000

    Change keybinding macro to acept a program alias
    A program alias allows a keybinding which uses command line options to
    have a simple name which is easy to remember

--- keybindings.lisp
+++ keybindings.lisp
@@ -9,31 +9,32 @@
 (define-key *root-map* (kbd "2") "vsplit")
 (define-key *root-map* (kbd "3") "hsplit")
 
-(defmacro make-program-binding (program-name window-class keybinding)
+(defmacro make-program-binding (program-name window-class keybinding &optional alias)
   "Create run-or-raise and run-or-pull commands for program-name
 window-class is the windows-class
 Also add keybinding to the commands. 
 C-keybinding r calls run-or-raise
 C-keybinding p calls run-or-pull
 C-keybinding n creates a new instance of the program"
-  
+  (if (not alias)
+      (setf alias program-name))
   `(progn
-     (defvar ,(intern (format nil "*~a-map*" program-name)) nil)
+     (defvar ,(intern (format nil "*~a-map*" alias)) nil)
 
-     (defcommand ,(intern (format nil "~a" program-name)) () () (run-shell-command ,program-name))
+     (defcommand ,(intern (format nil "~a" alias)) () () (run-shell-command ,program-name))
      
-     (defcommand ,(intern (format nil "run-or-raise-~a" program-name)) () ()
+     (defcommand ,(intern (format nil "run-or-raise-~a" alias)) () ()
 		 (run-or-raise ,program-name '(:class ,window-class)))
      
-     (defcommand ,(intern (format nil "run-or-pull-~a" program-name)) () ()
+     (defcommand ,(intern (format nil "run-or-pull-~a" alias)) () ()
 		 (run-or-pull ,program-name '(:class ,window-class)))
      
-     (fill-keymap ,(intern (format nil "*~a-map*" program-name))
-		  (kbd "p") ,(format nil "run-or-pull-~a" program-name)
-		  (kbd "r") ,(format nil "run-or-raise-~a" program-name)
-		  (kbd "n") ,(format nil "~a" program-name))
+     (fill-keymap ,(intern (format nil "*~a-map*" alias))
+		  (kbd "p") ,(format nil "run-or-pull-~a" alias)
+		  (kbd "r") ,(format nil "run-or-raise-~a" alias)
+		  (kbd "n") ,(format nil "~a" alias))
      
-     (define-key *root-map* (kbd ,keybinding) (intern ,(format nil "*~a-map*" program-name)))))
+     (define-key *root-map* (kbd ,keybinding) (intern ,(format nil "*~a-map*" alias)))))
 
 (make-program-binding "firefox" "Firefox" "f")
 
