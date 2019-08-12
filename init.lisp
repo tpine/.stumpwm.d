d69854645c50f1a122bcc7f9f79d9869306b46c3
Thomas Atkinson <tommy@experience.digital>
2019-08-12 11:46:12 +1000

    Set wmname for java

--- init.lisp
+++ init.lisp
@@ -27,6 +27,11 @@
 ;;; Set Time Input String
 (setf *time-format-string-default* "%a %b %e %Y %l:%M%P")
 
+;; JAVA has some issues with non reparenting window managers
+;; While we could set _JAVA_AWT_WM_NONREPARENTING=1 lets just say we are oracles tiling window manager
+;; I find that funnier
+(run-shell-command "wmname LG3D")
+
 ;; set module directory (if not already set)
 (set-module-dir "/home/thomas/.stumpwm.d/modules/")
 
