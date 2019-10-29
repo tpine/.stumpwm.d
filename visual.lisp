568d9b6382fd95bf1da561d2218fcff756526115
Thomas Atkinson <thomas@shalom.localdomain>
2019-10-29 19:24:29 +1100

    Add basic theming

--- visual.lisp
+++ visual.lisp
@@ -1,23 +1,94 @@
 (in-package :stumpwm)
 
 ;;; Visual
+(defvar *themes* (make-hash-table))
+(defun add-theme (name theme)
+  (setf (gethash name *themes*) theme))
+
 ;;; Colors based off spacemacs-dark-theme for emacs
+(defclass theme ()
+    ((fg
+      :initarg :fg
+      :type string)
+     (bg
+      :initarg :bg
+      :type string)
+
+     (border
+      :initarg :border
+      :type string)
+
+     (focus
+      :initarg :focus
+      :type string)
+     (unfocus
+      :initarg :unfocus
+      :type string)
+
+     (mode-line-fg
+      :initarg :mode-line-fg
+      :type string)
+     (mode-line-bg
+      :initarg :mode-line-bg
+      :type string)
+     (mode-line-border
+      :initarg :mode-line-border
+      :type string)
+
+     (color-map-first
+      :initarg :color-map-first
+      :type string)
+     (color-map-last
+      :initarg :color-map-last
+      :type string)))
+
+(defun apply-theme (theme)
+  (set-fg-color (slot-value theme 'fg))
+  (set-bg-color (slot-value theme 'bg))
+  (set-border-color (slot-value theme 'border))
+  (set-focus-color (slot-value theme 'focus))
+  (set-unfocus-color (slot-value theme 'unfocus))
+
+  (setf *mode-line-foreground-color* (slot-value theme 'mode-line-fg)
+	*mode-line-background-color* (slot-value theme 'mode-line-bg)
+	*mode-line-border-color* (slot-value theme 'mode-line-border))
+  
+  (setf (car *colors*) (slot-value theme 'color-map-first)
+	(car (last *colors*)) (slot-value theme 'color-map-last))
+  (update-color-map (current-screen)))
+
 (let ((grey "#292b2e")
       (purple "#5d4d7a"))
+  (add-theme 'spacemacs
+	     (make-instance 'theme
+	      :fg purple
+	      :bg grey
+	      :border purple
+	      :focus purple
+	      :unfocus grey
+	      :mode-line-fg purple
+	      :mode-line-bg grey
+	      :mode-line-border purple
+	      :color-map-first grey
+	      :color-map-last purple)))
 
-  (set-fg-color purple)
-  (set-bg-color grey)
-  (set-border-color purple)
-  (set-focus-color purple)
-  (set-unfocus-color grey)
+(let ((fg "#ebdbb2")
+      (bg "#282828")
+      (border "#665c54"))
+  (add-theme 'gruvbox
+	     (make-instance 'theme
+	      :fg fg
+	      :bg bg
+	      :border border
+	      :focus fg
+	      :unfocus bg
+	      :mode-line-fg fg
+	      :mode-line-bg bg
+	      :mode-line-border border
+	      :color-map-first bg
+	      :color-map-last fg)))
 
-  (setf *mode-line-foreground-color* purple
-	*mode-line-background-color* grey
-	*mode-line-border-color* purple)
-  
-  (setf (car *colors*) grey
-	(car (last *colors*)) purple)
-  (update-color-map (current-screen)))
+(apply-theme (gethash 'gruvbox *themes*))
 
 ;;; Load battery module
 (load-module "notify")
