c497c8e07c1487d146f92507e7a77c16e177cad4
Thomas Atkinson <tnatkinson95@gmail.com>
2020-07-15 22:46:04 +1000

    Get unread emails from gmail

--- visual.lisp
+++ visual.lisp
@@ -1,3 +1,12 @@
+(ql:quickload :bt-semaphore)
+(ql:quickload :drakma)
+(ql:quickload :cxml)
+(ql:quickload :clss)
+(ql:quickload :lquery)
+(ql:quickload :plump)
+(ql:quickload :xpath)
+(ql:quickload :yason)
+
 (in-package :stumpwm)
 
 ;;; Visual
@@ -112,11 +121,74 @@
 (xft:cache-fonts)
 (set-font (make-instance 'xft:font :family "DejaVu Sans Mono" :subfamily "Bold" :slant "r" :size 10))
 
+(defparameter *unread-emails* nil)
+(defparameter *updating-email-count* nil)
+
+(defvar *google-tokens* nil)
+
+
+(defun load-google-credentials ()
+  (with-open-file (stream ".google.lisp")
+     (with-standard-io-syntax
+       (setf *google-tokens* (read stream)))))
+
+(defun save-google-credentials ()
+  (with-open-file (out ".google.lisp"
+                   :direction :output
+                   :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *google-tokens* out))))
+
+(load-google-credentials)
+
+(defun refresh-access-token ()
+  (let ((refresh-token (getf *google-tokens* :refresh-token))
+	(client-id (getf *google-tokens* :client-id))
+	(client-secret (getf *google-tokens* :client-secret)))
+    (setf (getf *google-tokens* :access-token)
+	  (gethash
+	   "access_token"
+	   (yason:parse (drakma:http-request
+			 "https://accounts.google.com/o/oauth2/token"
+			 :method :post
+			 :parameters (list (cons "client_id" client-id)
+					   (cons "client_secret" client-secret)
+					   (cons "refresh_token" refresh-token)
+					   (cons "grant_type" "refresh_token"))
+			 :want-stream t))))))
+
+
+(defun get-unread-email-count (xml)
+  (format t "~a" xml)
+  (let ((doc (plump:parse xml)))
+    (plump:render-text (car (plump:get-elements-by-tag-name doc "fullcount")))))
+
 (defun get-unread-emails ()
-  (let ((emails (remove #\Newline (run-shell-command "notmuch count tag:unread" t))))
-    (if (equal emails "0")
-	"EMAILS"
-	"^[^3*EMAILS^]")))
+  (bt:make-thread
+   (lambda ()
+
+       (if (equal *updating-email-count* nil)
+	   (progn (setf *updating-email-count* t)
+		  (handler-case
+		      (let* ((request (multiple-value-list (drakma:http-request
+						      "https://mail.google.com/mail/feed/atom"
+						      :additional-headers (list (cons "Authorization" (format nil "Bearer ~a" (getf *google-tokens* :access-token)))))))
+			     (stream (nth 0 request)))
+			(cond ((equal (nth 1 request) 401)
+			       (refresh-access-token)
+			       (setf *unread-emails* nil
+				     *updating-email-count* nil))
+			      (t
+			       (setf *unread-emails* (get-unread-email-count stream)
+				     *updating-email-count* nil))))
+		    (usocket:ns-try-again-condition (c) ;; <-- optional argument
+		      (declare (ignore c))
+		      (setf *unread-emails* nil
+			    *updating-email-count* nil)))))))
+   
+  (cond ((equal *unread-emails* "0") "^[^2*EMAILS^]")
+	((not *unread-emails*) "^[^1*EMAILS^]")
+	(t (format nil "^[^3*~a EMAILS^]" *unread-emails*))))
 
 (defun battery-format (ml)
   (declare (ignore ml))
