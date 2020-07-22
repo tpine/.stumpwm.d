2a2a6ff791d0cf24d3b310cd93cb4f61b0cd1082
Thomas Atkinson <tnatkinson95@gmail.com>
2020-07-22 12:01:31 +1000

    Fix reload-init command

--- init.lisp
+++ init.lisp
@@ -7,12 +7,3 @@
 (set-module-dir "/home/thomas/.stumpwm.d/modules/")
 
 (asdf:operate 'asdf:load-op :stumpwm-init)
-
-
-(defun reload-init ()
-  "Restart Slynk and reload source.
-This is needed if Sly updates while StumpWM is running"
-  (slynk:stop-server 4004)
-  (load-rc-file)
-  (slynk:create-server :port 4004
-		     :dont-close t))
--- utility.lisp
+++ utility.lisp
@@ -1,9 +1,9 @@
-(in-package :stumpwm)
+(in-package :stumpwm-user)
 
 (defun reload-init ()
   "Restart Slynk and reload source.
 This is needed if Sly updates while StumpWM is running"
   (slynk:stop-server 4004)
-  (load-rc-file)
+  (stumpwm::load-rc-file)
   (slynk:create-server :port 4004
 		     :dont-close t))
